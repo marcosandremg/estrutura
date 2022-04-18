c7f404c7c9d10b19beffb2c5e9daf47f3cbb21ce
marco <marcos131962@yahoo.com.br>
2022-04-18 12:49:44 -0300

    prova

--- ods/arraydeque.py
+++ ods/arraydeque.py
@@ -23,46 +23,52 @@ class ArrayDeque(BaseList):
 
     def get(self, i):
         if i < 0 or i >= self.n: raise IndexError()
-        return self.a[(i+self.j)%len(self.a)]
+        return self.a[(i + self.j) % len(self.a)]
 
     def set(self, i, x):
         if i < 0 or i >= self.n: raise IndexError()
-        y = self.a[(i+self.j)%len(self.a)]
-        self.a[(i+self.j)%len(self.a)] = x
+        y = self.a[(i + self.j) % len(self.a)]
+        self.a[(i + self.j) % len(self.a)] = x
         return y
 
-    def add(self, i, x): 
+    def add(self, i, x):
         if i < 0 or i > self.n: raise IndexError()
         if self.n == len(self.a): self._resize()
-        if i < self.n/2:
-            self.j = (self.j-1) % len(self.a)
+        if i < self.n / 2:
+            self.j = (self.j - 1) % len(self.a)
             for k in range(i):
-                self.a[(self.j+k)%len(self.a)] = self.a[(self.j+k+1)%len(self.a)]
+                self.a[(self.j + k) % len(self.a)] = self.a[(self.j + k + 1) % len(self.a)]
         else:
             for k in range(self.n, i, -1):
-                self.a[(self.j+k)%len(self.a)] = self.a[(self.j+k-1)%len(self.a)]
-        self.a[(self.j+i)%len(self.a)] = x
+                self.a[(self.j + k) % len(self.a)] = self.a[(self.j + k - 1) % len(self.a)]
+        self.a[(self.j + i) % len(self.a)] = x
         self.n += 1
 
-    def remove(self, i): 
+    def remove(self, i):
         if i < 0 or i >= self.n: raise IndexError()
-        x = self.a[(self.j+i)%len(self.a)]
+        x = self.a[(self.j + i) % len(self.a)]
         if i < self.n / 2:
             for k in range(i, 0, -1):
-                self.a[(self.j+k)%len(self.a)] = self.a[(self.j+k-1)%len(self.a)]
-            self.j = (self.j+1) % len(self.a)
+                self.a[(self.j + k) % len(self.a)] = self.a[(self.j + k - 1) % len(self.a)]
+            self.j = (self.j + 1) % len(self.a)
         else:
-            for k in range(i, self.n-1): 
-                self.a[(self.j+k)%len(self.a)] = self.a[(self.j+k+1)%len(self.a)]
+            for k in range(i, self.n - 1):
+                self.a[(self.j + k) % len(self.a)] = self.a[(self.j + k + 1) % len(self.a)]
         self.n -= 1
-        if len(self.a) >= 3*self.n: self._resize()
+        if len(self.a) >= 3 * self.n: self._resize()
         return x
-  
+
     def _resize(self):
-        b = new_array(max(1, 2*self.n))
+        b = new_array(max(1, 2 * self.n))
         for k in range(self.n):
-            b[k] = self.a[(self.j+k)%len(self.a)]
+            b[k] = self.a[(self.j + k) % len(self.a)]
         self.a = b
         self.j = 0
-    
 
+    def remove_varios(self, start, stop):
+        if (start or stop) < 0 or (start or stop) >= self.n: raise IndexError()
+        k = stop - start + 1
+        if start < self.n / 2:
+            self.a[(self.j + k) % len(self.a)] = self.a[start % len(self.a)]
+        else:
+            self.a[(self.j + k) % len(self.a)] = self.a[(self.j + k + 1) % len(self.a)]
