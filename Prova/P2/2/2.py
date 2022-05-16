44145f369e67ea8db741da58e5eb46f8d124d36d
marco <marcos131962@yahoo.com.br>
2022-05-16 17:38:27 -0300

    p2

---
+++ Prova/P2/2/2.py
@@ -0,0 +1,15 @@
+from ods.binarysearchtree import BinarySearchTree
+
+a = BinarySearchTree()
+a.add(50)
+a.add(71)
+a.add(38)
+a.add(80)
+a.add(75)
+a.add(59)
+a.add(55)
+a.add(40)
+a.add(39)
+a.add(27)
+
+print(a.count_left())
