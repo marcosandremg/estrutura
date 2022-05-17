3995bdd34a1553d34109654b77fde395eba4b8ba
marco <marcos131962@yahoo.com.br>
2022-05-17 04:41:24 -0300

    all

--- Prova/P2/4/4.py
+++ Prova/P2/4/4.py
@@ -6,3 +6,10 @@ realizado sem ajuda de qualquer outra pessoa, e constitui solução original cri
 sem auxílio de terceiros ou cópia de trabalhos já publicados.
 
 """
+from ods.binaryheap import BinaryHeap
+
+a = BinaryHeap()
+for _ in range(15):
+    a.add(_)
+print(a)
+print(a.remove_two(1))
--- ods/binaryheap.py
+++ ods/binaryheap.py
@@ -1,19 +1,30 @@
 """An implemnetation of a binary heap using an array"""
 from ods.utils import new_array
 from ods.base import BaseSet
+"""
+Nome: Marcos André de Moraes Galdino
+Matricula: 201810068311
+Declaro que a resolução desta atividade, que submeto para avaliação, é meu trabalho individual,
+realizado sem ajuda de qualquer outra pessoa, e constitui solução original criada por mim,
+sem auxílio de terceiros ou cópia de trabalhos já publicados.
+
+"""
 
 def left(i):
-    return 2*i + 1
+    return 2 * i + 1
+
 
 def right(i):
-    return 2*(i+1)
+    return 2 * (i + 1)
+
 
 def parent(i):
-    return (i-1)//2
+    return (i - 1) // 2
+
 
 class BinaryHeap(BaseSet):
     def __init__(self, iterable=[], a=None):
-        if a: 
+        if a:
             self._make_heap(a)
         else:
             self._initialize()
@@ -22,9 +33,9 @@ class BinaryHeap(BaseSet):
     def _make_heap(self, a):
         self.a = a
         self.n = len(a)
-        for i in range(self.n//2-1, -1, -1):
+        for i in range(self.n // 2 - 1, -1, -1):
             self.trickle_down(i)
-            
+
     def _initialize(self):
         self.a = new_array(1)
         self.n = 0
@@ -32,17 +43,17 @@ class BinaryHeap(BaseSet):
     clear = _initialize
 
     def resize(self):
-        b = new_array(max(2*self.n, 1))
+        b = new_array(max(2 * self.n, 1))
         for i in range(self.n):
             b[i] = self.a[i]
-        self.a = b    
+        self.a = b
 
     def add(self, x):
-        if len(self.a) < self.n+1:
+        if len(self.a) < self.n + 1:
             self.resize()
         self.a[self.n] = x
         self.n += 1
-        self.bubble_up(self.n-1)
+        self.bubble_up(self.n - 1)
         return True
 
     def bubble_up(self, i):
@@ -54,10 +65,10 @@ class BinaryHeap(BaseSet):
 
     def remove(self):
         x = self.a[0]
-        self.a[0] = self.a[self.n-1]
+        self.a[0] = self.a[self.n - 1]
         self.n -= 1
         self.trickle_down(0)
-        if 3*self.n < len(self.a):
+        if 3 * self.n < len(self.a):
             self.resize()
         return x
 
@@ -76,10 +87,8 @@ class BinaryHeap(BaseSet):
                 if l < self.n and self.a[l] < self.a[i]:
                     j = l
             if j >= 0:
-                self.a[j], self.a[i] =     self.a[i], self.a[j]
+                self.a[j], self.a[i] = self.a[i], self.a[j]
             i = j
-
-
 
     def find_min(self):
         if n == 0: raise IndexError()
@@ -89,4 +98,19 @@ class BinaryHeap(BaseSet):
         for i in range(self.n):
             yield self.a[i]
 
+    def remove_two(self, i):
+        self.a[i] = self.a[0]
+        self.n -= 1
+        i = 0
+        while self.a[left(i)] and self.a[right(i)] <= self.a[i]:
+            if self.a[left(i)] < self.a[right(i)]:
+                self.a[parent(i)]= self.a[left(i)]
+                i +=1
+            if self.a[left(i)] > self.a[right(i)]:
+                self.a[parent(i)]= self.a[right(i)]
+                i +=2
 
+        if 3 * self.n < len(self.a):
+            self.resize()
+
+        print(self.a)
